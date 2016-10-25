fb94df43331f1ed635950fcceb546365478a6525
Saif Shaikh <t-mohamshaikh@expedia.com>
2016-10-25 13:44:24 +0100

    Session 6 Prep

---
+++ session6.py
@@ -0,0 +1,14 @@
+# New Data Type to store things.
+
+my_list = [] #This is the declaration for the list (creation of the list).
+my_list.append("item") #This adds whatever you pass in to the end of the list using the append function
+my_list.append("hello")
+my_list.append("world")
+
+#Another way of adding something to the list is by using something called an index.
+#You can specify an index using a number.
+
+my_list[0] = "Hi"
+my_list[1] = "Ola"
+print my_list
+print len(my_list)
